d9f7716fa48ea42e610fbc8c144ba035a18c932e
GautamJadhav123 <gautamjadhav12597@gmail.com>
2023-03-25 21:16:04 +0530

    first commit

---
+++ Git - Practice 1/src/com/gitpractice/gitpractice1/demo/MathOperations.java
@@ -0,0 +1,15 @@
+package com.gitpractice.gitpractice1.demo;
+
+public class MathOperations {
+
+	public static void main(String[] args) {
+		
+		
+			int x = 5;
+			int y = 10;
+			
+			
+			System.out.println("addition is --> " + (x +y) );
+	}
+
+}
